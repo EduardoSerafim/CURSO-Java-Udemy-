cf845b9a2278ffa439463d300dbeca52a10d9e66
Eduardo Serafim <eduserafim7@gmail.com>
2022-06-20 15:50:05 -0300

    Exercicio matriz resolvido

--- SEC10_ArraysListasPt3Exercicio/src/app/Main.java
+++ SEC10_ArraysListasPt3Exercicio/src/app/Main.java
@@ -17,6 +17,31 @@ public class Main {
 			}
 		}
 		
+		int x = sc.nextInt();
+		
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				if(matriz[i][j] == x) {
+					System.out.printf("Position %d,%d:\n", i, j);
+					if(j > 0) {
+						System.out.printf("Left: %d\n" ,matriz[i][j-1]);
+					}
+					if(i > 0) {
+						System.out.printf("Up: %d\n" ,matriz[i-1][j]);						
+					}
+					if(j < matriz[i].length-1) {
+						System.out.printf("Right: %d\n" ,matriz[i][j+1]);
+					}					
+					if(i < matriz.length-1) {
+						System.out.printf("Down: %d\n" ,matriz[i+1][j]);
+					}
+					
+					
+				}
+			}
+		}
+		
+		
 		sc.close();
 	}
 
