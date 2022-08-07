d6ec36e015dbb6b9c49bfd24334c402cccb46e51
Eduardo Serafim <eduserafim7@gmail.com>
2022-08-07 00:25:22 -0300

    SEC 18 parte 2 - solução com interface

---
+++ SEC18_InterfacesPt2/src/model/services/RentalService.java
@@ -0,0 +1,35 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+
+	private Double pricePerHour;
+	private Double pricePerDay;
+	
+	private TaxService taxServices; 
+	
+	public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxServices) {
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+		this.taxServices = taxServices;
+	}
+
+	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+		double hours = (double)(t2 - t1)/1000 / 60 / 60;
+		
+		double basicPayment; 
+		if(hours <= 12) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+		}else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;			
+		}
+		
+		double tax = taxServices.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+}
